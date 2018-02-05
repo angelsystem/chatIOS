2ee330cd4e65203742145f4c1576bb715c52f49e
angelsystem <angelsystem1387@gmail.com>
2018-02-05 02:26:25 -0300

    first commit

---
+++ Pods/Target Support Files/Pods-chatWeClub/Pods-chatWeClub-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chatWeClub : NSObject
+@end
+@implementation PodsDummy_Pods_chatWeClub
+@end
---
+++ Pods/Target Support Files/Pods-chatWeClubUITests/Pods-chatWeClubUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chatWeClubUITests : NSObject
+@end
+@implementation PodsDummy_Pods_chatWeClubUITests
+@end
